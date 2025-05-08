fc552858daf1cc6b102b1ffe12e8cd6b8de8da34
CHOR NARIN <naritkh178@gmail.com>
2025-05-08 22:22:21 +0700

    fix error

--- src/main/java/chornarin/com/kh/Midterm/controller/HelloController.java
+++ src/main/java/chornarin/com/kh/Midterm/controller/HelloController.java
@@ -1,13 +1,13 @@
-package Midterm.com.kh.controller;
+// package Midterm.com.kh.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+// @RestController
+// public class HelloController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World!";
-    }
-}
+//     @GetMapping("/hello")
+//     public String sayHello() {
+//         return "Hello World!";
+//     }
+// }
